5d688175d9857fb3fe01f89fc0dff160fe6f4739
jsndz <jaisondz9360.com>
2024-10-10 23:17:08 +0530

    projects 2

--- components/projects/projects.tsx
+++ components/projects/projects.tsx
@@ -11,6 +11,7 @@ const Projects = () => {
     </div>
   );
 };
+
 const DummyContent = () => {
   return (
     <>
@@ -45,41 +46,253 @@ const DummyContent = () => {
 
 const data = [
   {
-    category: "Artificial Intelligence",
-    title: "You can do more with AI.",
+    category: "Web Application",
+    title: "Noatric",
     src: "https://images.unsplash.com/photo-1593508512255-86ab42a8e620?q=80&w=3556&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
+    content: (
+      <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+        <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+          Experience the Marvels of Kinetic Wonders with Noatric. Unleash Your
+          Space's Potential and Elevate Your Environment with Noatric's Kinetic
+          Collection, Where Art Meets Science Fiction. This project has been
+          invaluable in learning about hosting, React Redux, Stripe integration,
+          cloud databases, and the overall process of building a functional
+          website.
+        </p>
+        <Image
+          src="https://assets.aceternity.com/macbook.png"
+          alt="Macbook mockup from Aceternity UI"
+          height="500"
+          width="500"
+          className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+        />
+      </div>
+    ),
   },
   {
-    category: "Productivity",
-    title: "Enhance your productivity.",
+    category: "Web Application",
+    title: "SkySense",
     src: "https://images.unsplash.com/photo-1531554694128-c4c6665f59c2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
+    content: (
+      <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+        <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+          SkySense is a web application that provides weather data and city
+          information
+        </p>
+        <Image
+          src="https://assets.aceternity.com/macbook.png"
+          alt="Macbook mockup from Aceternity UI"
+          height="500"
+          width="500"
+          className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+        />
+      </div>
+    ),
   },
   {
-    category: "Product",
-    title: "Launching the new Apple Vision Pro.",
+    category: "Web Application",
+    title: "Ephemera",
     src: "https://images.unsplash.com/photo-1713869791518-a770879e60dc?q=80&w=2333&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
+    content: (
+      <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+        <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+          Ephemera is a transient, secure, and private communication web app
+          designed for users who value privacy in their conversations.
+        </p>
+        <Image
+          src="https://assets.aceternity.com/macbook.png"
+          alt="Macbook mockup from Aceternity UI"
+          height="500"
+          width="500"
+          className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+        />
+      </div>
+    ),
   },
 
   {
-    category: "Product",
-    title: "Maps for your iPhone 15 Pro Max.",
+    category: "Software Development",
+    title: "Building a SQLite with C",
     src: "https://images.unsplash.com/photo-1599202860130-f600f4948364?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
+    content: (
+      <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+        <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+          A project focused on building a lightweight SQLite-like database using
+          C.
+        </p>
+        <Image
+          src="https://assets.aceternity.com/macbook.png"
+          alt="Macbook mockup from Aceternity UI"
+          height="500"
+          width="500"
+          className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+        />
+      </div>
+    ),
   },
   {
-    category: "iOS",
-    title: "Photography just got better.",
+    category: "Software Development",
+    title: "CLI_Template.js",
     src: "https://images.unsplash.com/photo-1602081957921-9137a5d6eaee?q=80&w=2793&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
+    content: (
+      <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+        <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+          A working skeleton for a command line tool. We created the binary,
+          processed the CLI arguments, found and validated the configuration,
+          and created a way to communicate with our users via logs.
+        </p>
+        <Image
+          src="https://assets.aceternity.com/macbook.png"
+          alt="Macbook mockup from Aceternity UI"
+          height="500"
+          width="500"
+          className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+        />
+      </div>
+    ),
   },
   {
-    category: "Hiring",
-    title: "Hiring for a Staff Software Engineer",
+    category: "Web Application",
+    title: "PrimeBoard",
     src: "https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
+    content: (
+      <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+        <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+          A leaderboard for top products of PRIME 2024 from Innoventure SJEC.
+          Prime 2024 is the ultimate platform for showcasing the top products of
+          the year. The leaderboard showed real-time data of the top products.
+        </p>
+        <Image
+          src="https://assets.aceternity.com/macbook.png"
+          alt="Macbook mockup from Aceternity UI"
+          height="500"
+          width="500"
+          className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+        />
+      </div>
+    ),
+  },
+  {
+    category: "Web Application",
+    title: "Dept-Emp",
+    src: "https://images.unsplash.com/photo-1593508512255-86ab42a8e620?q=80&w=3556&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    content: (
+      <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+        <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+          A DBMS mini-project used to describe the relation between a Employee
+          and Departments.
+        </p>
+        <Image
+          src="https://assets.aceternity.com/macbook.png"
+          alt="Macbook mockup from Aceternity UI"
+          height="500"
+          width="500"
+          className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+        />
+      </div>
+    ),
+  },
+  {
+    category: "IoT Development",
+    title: "v4-Parking-Service",
+    src: "https://images.unsplash.com/photo-1531554694128-c4c6665f59c2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    content: (
+      <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+        <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+          v4-Parking-Service is a project that uses IoT to manage parking. It
+          consists of arduino, ultrasonic sensor, and a buzzer. The sensor
+          detects the distance of the vehicle from the wall and the buzzer beeps
+          when the vehicle is too close to the wall. And the amount is
+          calculated and mailed to the user.
+        </p>
+        <Image
+          src="https://assets.aceternity.com/macbook.png"
+          alt="Macbook mockup from Aceternity UI"
+          height="500"
+          width="500"
+          className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+        />
+      </div>
+    ),
+  },
+  {
+    category: "Web Application",
+    title: "HauntHub",
+    src: "https://images.unsplash.com/photo-1713869791518-a770879e60dc?q=80&w=2333&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    content: (
+      <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+        <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+          "Not Developed Yet." A website to share scary stuff.
+        </p>
+        <Image
+          src="https://assets.aceternity.com/macbook.png"
+          alt="Macbook mockup from Aceternity UI"
+          height="500"
+          width="500"
+          className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+        />
+      </div>
+    ),
+  },
+
+  {
+    category: "Backend Development",
+    title: "SnapShort",
+    src: "https://images.unsplash.com/photo-1599202860130-f600f4948364?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    content: (
+      <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+        <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+          SnapShort is a URL shortening service that allows users to create
+          short and shareable links.
+        </p>
+        <Image
+          src="https://assets.aceternity.com/macbook.png"
+          alt="Macbook mockup from Aceternity UI"
+          height="500"
+          width="500"
+          className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+        />
+      </div>
+    ),
+  },
+  {
+    category: "Backend Development",
+    title: "SnapStory",
+    src: "https://images.unsplash.com/photo-1602081957921-9137a5d6eaee?q=80&w=2793&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    content: (
+      <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+        <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+          Snapstory is a backend project that allows users to upload and share
+          short blog posts.
+        </p>
+        <Image
+          src="https://assets.aceternity.com/macbook.png"
+          alt="Macbook mockup from Aceternity UI"
+          height="500"
+          width="500"
+          className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+        />
+      </div>
+    ),
+  },
+  {
+    category: "Backend Development",
+    title: "Twitter backend clone",
+    src: "https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    content: (
+      <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+        <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+          Twitter backend clone.
+        </p>
+        <Image
+          src="https://assets.aceternity.com/macbook.png"
+          alt="Macbook mockup from Aceternity UI"
+          height="500"
+          width="500"
+          className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+        />
+      </div>
+    ),
   },
 ];
 
